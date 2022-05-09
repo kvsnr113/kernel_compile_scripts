e2c6b56a0014e0a3428e7f26355179394ae143d6
kvsnr113 <105036399+kvsnr113@users.noreply.github.com>
2022-05-09 22:41:17 +0700

    Improve compile/build script

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 export TC_DIR="$HOME/tc/compiler"
 export PATH="$TC_DIR/bin:$PATH"
 
-export DEVICE="Poco X3 Pro (vayu)"
-export ZIPNAME="kernel-vayu-$(date '+%Y%m%d-%H%M').zip"
+export DEVICE="Poco X3 Pro"
+export ZIPNAME="R.Y.N-kernel-vayu-$(date '+%Y%m%d-%H%M').zip"
 export DEFCONFIG="vayu_defconfig"
 
 export GITLOG=$(git log --pretty=format:'"%h : %s"' -1)
-export KBUILD_BUILD_USER=113
-export KBUILD_BUILD_HOST=DonateSerperSar!
+export KBUILD_BUILD_USER=kvsnr113
+export KBUILD_BUILD_HOST=Project113
 
 export CHATID=-1001586260532
 export TOKEN=5382711200:AAFp0g3MrphAUgylIq8ynMAbfeOys8lzWTI
@@ -25,7 +25,7 @@ curl -s -X POST https://api.telegram.org/bot"${TOKEN}"/sendMessage \
 curl -s -X POST https://api.telegram.org/bot"${TOKEN}"/sendMessage \
 		-d parse_mode="Markdown" \
 		-d chat_id="$CHATID" \
-		-d text="Compiler : $COMPILER || $KBUILD_BUILD_USER-$KBUILD_BUILD_HOST"
+		-d text="Device : $DEVICE || Compiler : $COMPILER || Builder : $KBUILD_BUILD_USER-$KBUILD_BUILD_HOST"
 
 curl -s -X POST https://api.telegram.org/bot"${TOKEN}"/sendMessage \
 		-d parse_mode="Markdown" \
@@ -55,20 +55,23 @@ mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz dtbo.img
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
 
 kernel="out/arch/arm64/boot/Image.gz"
-dtb="out/arch/arm64/boot/dts/qcom/sm8150-v2.dtb"
 dtbo="out/arch/arm64/boot/dtbo.img"
 
-if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
-	zip -r9 "../$ZIPNAME" $kernel $dtbo $dtb
+if [ -f "$kernel" ] && [ -f "$dtbo" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	cp $kernel $dtbo AnyKernel3
+	cd AnyKernel3 || exit
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
 	cd ..
+	echo -e "\nCompleted !"
 	curl -F document=@$ZIPNAME https://api.telegram.org/bot"${TOKEN}"/sendDocument \
         -F chat_id="$CHATID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Build Success"
+        -F caption="Build Success - $DEVICE"
 	echo
 else
 	echo -e "\nCompilation failed!"
